8d233daaf4c1931cdefec2a4301738c658fa0f1a
marie <mariecaron27@gmail.com>
2026-06-24 11:08:59 +0200

    Reduce the size of the left controller

--- vertical_split/obj/Debug/net10.0-macos/osx-arm64/vertical_split.AssemblyInfo.cs
+++ vertical_split/obj/Debug/net10.0-macos/osx-arm64/vertical_split.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vertical_split")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c241f28a322c84c8f00f312db97b672c5768d14c")]
 [assembly: System.Reflection.AssemblyProductAttribute("vertical_split")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vertical_split")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
